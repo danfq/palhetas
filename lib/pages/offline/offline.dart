b4d2310408433244b50c656653f7c467dc9751ca
DanFQ <danfq.dev@gmail.com>
2024-03-09 13:14:38 +0000

    Added 'Remove from Offline'

--- lib/pages/offline/offline.dart
+++ lib/pages/offline/offline.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
 import 'package:palhetas/util/data/local.dart';
 import 'package:palhetas/util/models/article.dart';
 
-class Offline extends StatelessWidget {
+class Offline extends StatefulWidget {
   const Offline({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    //Offline News
-    final List offlineNews = LocalData.boxData(box: "offline")["items"] ?? [];
+  State<Offline> createState() => _OfflineState();
+}
 
-    //UI
+class _OfflineState extends State<Offline> {
+  //Offline News
+  final List offlineNews = LocalData.boxData(box: "offline")["items"] ?? [];
+
+  @override
+  Widget build(BuildContext context) {
     return offlineNews.isNotEmpty
         ? ListView.builder(
             itemCount: offlineNews.length,
@@ -19,7 +24,37 @@ class Offline extends StatelessWidget {
               final article = Article.fromJSON(offlineNews[index]);
 
               //UI
-              return article;
+              return GestureDetector(
+                onLongPress: () async {
+                  //Confirmation
+                  await Get.defaultDialog(
+                    title: "Remover de Offline",
+                    content: const Text("Remover este Artigo de Offline?"),
+                    cancel: TextButton(
+                      onPressed: () => Get.back(),
+                      child: const Text("Cancelar"),
+                    ),
+                    confirm: ElevatedButton(
+                      onPressed: () async {
+                        //Remove Article
+                        offlineNews.removeAt(index);
+
+                        //Save New List
+                        LocalData.boxData(box: "offline")["items"] =
+                            offlineNews;
+
+                        //Update UI
+                        setState(() {});
+
+                        //Close Dialog
+                        Get.back();
+                      },
+                      child: const Text("Remover"),
+                    ),
+                  );
+                },
+                child: article,
+              );
             },
           )
         : const Center(child: Text("Não Tens Artigos Guardados"));
