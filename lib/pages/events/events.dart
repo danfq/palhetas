a5045429c5b5aa10cc93e49f41d1001d82ae9e38
DanFQ <danfq.dev@gmail.com>
2024-05-11 19:23:33 +0100

    Events UI Update (Carousel Slider)

--- lib/pages/events/events.dart
+++ lib/pages/events/events.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 import 'package:palhetas/pages/events/single.dart';
@@ -18,29 +19,35 @@ class Events extends StatelessWidget {
 
           //Check Events
           if (events != null && events.isNotEmpty) {
-            return GridView.count(
-              physics: const BouncingScrollPhysics(),
-              crossAxisCount: 2,
-              mainAxisSpacing: 20.0,
-              crossAxisSpacing: 20.0,
-              children: events.map((event) {
-                return GestureDetector(
-                  onTap: () async {
-                    await Get.to(() => SingleEvent(imageURL: event.imageURL));
-                  },
-                  child: Hero(
-                    tag: event.imageURL,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(14.0),
-                      child: Image.network(
-                        event.imageURL,
-                        fit: BoxFit.cover,
-                        filterQuality: FilterQuality.high,
+            return Center(
+              child: CarouselSlider(
+                items: events
+                    .map(
+                      (event) => GestureDetector(
+                        onTap: () => Get.to(
+                          () => SingleEvent(imageURL: event.imageURL),
+                        ),
+                        child: Hero(
+                          tag: event.imageURL,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(14.0),
+                            child: Image.network(
+                              event.imageURL,
+                              fit: BoxFit.cover,
+                              filterQuality: FilterQuality.high,
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                );
-              }).toList(),
+                    )
+                    .toList(),
+                options: CarouselOptions(
+                  height: 500.0,
+                  viewportFraction: 0.7,
+                  enlargeCenterPage: true,
+                  enableInfiniteScroll: false,
+                ),
+              ),
             );
           } else {
             return const Center(child: Text("Nenhum Evento"));
